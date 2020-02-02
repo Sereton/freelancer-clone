d1a3ec8b772b7821f40225ad1c3b798a35c79d32
Jay Hernandez <ajuan_jose@hotmail.com>
2020-02-02 09:35:46 -0600

    small improvements suggested by Codacy

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,12 @@
 class OrdersController < ApplicationController
     before_action :authenticate_user!
     def create
-        gig = Gig.find(params[:gig_id])
-        pricing = gig.pricings.find_by(pricing_type: params[:pricing_type])
+        @gig = Gig.find(params[:gig_id])
+        pricing = @gig.pricings.find_by(pricing_type: params[:pricing_type])
         
 
-        if(pricing && !gig.has_single_pricing) || (pricing && pricing.basic? && gig.has_single_pricing)
-            charge(gig, pricing)
+        if(pricing && !@gig.has_single_pricing) || (pricing && pricing.basic? && @gig.has_single_pricing)
+            charge(@gig, pricing)
         else
             flash[:alert]= "Price is incorrect"
         end
@@ -23,8 +23,8 @@ class OrdersController < ApplicationController
     end
 
     def complete
-        order = Order.find(params[:id])
-        if !order.completed?
+        @order = Order.find(params[:id])
+        unless @order.completed?
             if order.completed!
                 flash[:notice] = "Your order was completed successfully"
             else
@@ -39,7 +39,7 @@ class OrdersController < ApplicationController
 
     def charge(gig,pricing)
         order = gig.orders.new
-        order.due_date = Date.today() + pricing.delivery_time.days
+        order.due_date = Time.zone.today() + pricing.delivery_time.days
         order.title = gig.title
         order.seller_name = gig.user.full_name
         order.buyer_name = current_user.full_name
